a9be65448e5dcb4ea40599cf9fbcb45ef8af7a31
that-kimani <that.kimani@gmail.com>
2025-05-05 23:55:45 +0300

    Added StaffUser model with key employee data and test entry
    - Created StaffUser model to store staff data; employee_id, names, job title, department, CTC, contact info, and joining date
    - Successfully added a test user to verify database integration

--- staff/models.py
+++ staff/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class StaffUser(models.Model):
+    employee_id = models.CharField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    job_title = models.CharField(max_length=100)
+    department = models.CharField(max_length=100)
+    monthly_ctc = models.IntegerField()
+    date_of_joining = models.DateField()
+    email = models.EmailField()
+    contact = models.CharField(max_length=15)
+    # profile_photo = models.ImageField(upload_to='upload_path e.g staff_photos/',null=True ,blank=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.employee_id})"
