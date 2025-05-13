73cdf9b1952196ba328be4387e1365d11043eab2
that-kimani <that.kimani@gmail.com>
2025-05-13 21:05:59 +0300

    Added a custom UserAccount model with custom user management.This way, we can use a custom model to authenticate users and categorise them.

--- api/models.py
+++ api/models.py
@@ -1,7 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class UserAccount(AbstractUser):
+class CustomUserManager(BaseUserManager):
+    def create_user(self , employee_id , password=None , **extra_fields):
+        if not employee_id:
+            raise ValueError("The Employee ID must be set.")
+        
+        user = self.model(employee_id=employee_id , **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+    
+    def create_superuser(self , employee_id, password=None , **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        user = self.create_user(employee_id, password, **extra_fields)
+
+        return user
+    
+
+class UserAccount(AbstractUser , PermissionsMixin):
+    employee_id = models.CharField(max_length=8, unique=True)
+    email = models.EmailField(blank=True)
     staff_category = models.CharField(max_length=100)
 
+    USERNAME_FIELD = 'employee_id'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.employee_id
+    
